f17884db3002423f829a231fab6e9a20848e49b0
Gao   pro <pro@prodemacbook-pro.local>
2016-02-29 19:22:57 +0800

    新建项目

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by gv pro on 16/2/29.
+//  Copyright © 2016年 gv pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by gv pro on 16/2/29.
+//  Copyright © 2016年 gv pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
